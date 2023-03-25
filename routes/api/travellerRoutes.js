7dfcb8380f4117fec958362e01c2facb082728db
Josh Taylor <jtaylor4@bootcampspot.com>
2023-03-25 14:56:30 -0700

    working create and delete routes for travellers

--- routes/api/travellerRoutes.js
+++ routes/api/travellerRoutes.js
@@ -17,14 +17,44 @@ router.get('/:id', async (req, res) => {
             include: [{ model: Location, through: Trip, as: 'planned_trips' }]
         });
 
-        if(!travellerData) {
-            res.status(404).json({ message: 'No traveller found with this id!'});
+        if (!travellerData) {
+            res.status(404).json({ message: 'No traveller found with this id!' });
             return;
         }
 
         res.status(200).json(travellerData);
     } catch (error) {
         res.status(500).json(error);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const travellerData = await Traveller.create(req.body);
+        res.status(200).json(travellerData);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const travellerData = await Traveller.destroy(
+            {
+                where:
+                {
+                    id: req.params.id
+                }
+            })
+
+        if (!travellerData) {
+            res.status(404).json({ message: 'No traveller found with this id!' });
+            return;
+        }
+
+        res.status(200).json(travellerData);
+    } catch (error) {
+        res.status(500).json(travellerData);
     }
 })
 
